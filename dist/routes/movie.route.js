c70ec55d73c0da9ba28aca8476df179e36ebafa0
Max Karpinsky <karpinsky@chapman.edu>
2023-12-09 19:12:00 -0800

    Remove ignored files

--- dist/routes/movie.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const movie_controller_1 = require("../controller/movie.controller");
-const movieRouter = (0, express_1.Router)();
-movieRouter.get("/search", movie_controller_1.default.searchMovies);
-movieRouter.get("/genre", movie_controller_1.default.searchMoviesByGenreScore);
-movieRouter.get("/details", movie_controller_1.default.getMovieDetails);
-exports.default = movieRouter;
--- dist/routes/review.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const review_controller_1 = require("../controller/review.controller");
-const validateReview_1 = require("../middlewares/validateReview");
-const reviewRouter = (0, express_1.Router)();
-reviewRouter.post("/add", validateReview_1.default, review_controller_1.default.addNewReview);
-reviewRouter.get("/", review_controller_1.default.getReviews);
-exports.default = reviewRouter;
